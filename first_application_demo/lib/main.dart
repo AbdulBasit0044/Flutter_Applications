dba0febb939875df37df0300dd3c4d4471deb4d0
AbdulBasit0044 <abdulbasit0044@gmail.com>
2020-01-22 21:43:51 +0500

    done with this

--- first_application_demo/lib/main.dart
+++ first_application_demo/lib/main.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import './views/video_cell.dart';
 
 void main() => runApp(FirstApp());
 
-class FirstApp extends StatefulWidget{
+class FirstApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new FirstAppState();
   }
 }
 
-class FirstAppState extends State<FirstApp>{
-
+class FirstAppState extends State<FirstApp> {
   var _isLoading = true;
+
+  var videos;
+  //Complete fetching data code to refer for future use
+  _fetchData() async {
+    print("Fetching data from network");
+    final url = "https://api.letsbuildthatapp.com/youtube/home_feed";
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      //print(response.body);
+
+      final map = json.decode(response.body);
+
+      final videoJson = map["videos"];
+      // videoJson.forEach((video){
+      //   print(video["name"]);
+      // });
+
+      setState(() {
+        _isLoading = false;
+        this.videos = videoJson;
+      });
+      //print(map["videos"]);
+    }
+    //print(response);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,19 +48,38 @@ class FirstAppState extends State<FirstApp>{
         appBar: new AppBar(
           title: new Text("Real World Application is here!!"),
           actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.refresh),
-            onPressed: (){
-              print("Reloading...");
-              setState(() {
-                _isLoading = false;
-              });
-            },)
+            new IconButton(
+              icon: new Icon(Icons.refresh),
+              onPressed: () {
+                print("Reloading...");
+                setState(() {
+                  _isLoading = true;
+                });
+                _fetchData();
+              },
+            )
           ],
         ),
         body: new Center(
-          child: _isLoading ? new CircularProgressIndicator():
-          new Text("Finished Loading..."),
-          ),
+          child: _isLoading
+              ? new CircularProgressIndicator()
+              : new ListView.builder(
+                  itemCount: this.videos != null ? this.videos.length : 0,
+                  itemBuilder: (context, i) {
+                    final video = this.videos[i];
+                    return new FlatButton(
+                      padding: new EdgeInsets.all(0.0),
+                      child: new VideoCell(video),
+                      onPressed: (){
+                        print(("Video cell is tapped"));
+                      },
+                    );
+                    return new VideoCell(video);
+
+                    //return new Text("Row: $i");
+                  },
+                ),
+        ),
       ),
     );
   }
