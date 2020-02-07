bda49d89547c4dd01b6ee16e53eb102a7d7eb55c
AbdulBasit0044 <abdulbasit0044@gmail.com>
2020-02-07 12:59:06 +0500

    Nothing

--- flutter_applications_the_net_ninja/myapp/lib/main.dart
+++ flutter_applications_the_net_ninja/myapp/lib/main.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey you!?'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('this is my second app'),
+      centerTitle: true,
+      backgroundColor: Colors.deepOrange[600],
+    ),
+    body: Center(
+      child: Text(
+        'This is the childish text',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.blueAccent,
+        ),
+      ),
+
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      child: Text("Click"),
+      backgroundColor:Colors.deepOrange[600],
+    ),
+  )
 ));
 
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
