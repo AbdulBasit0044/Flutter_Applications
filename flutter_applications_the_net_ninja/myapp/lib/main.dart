7232a71ce1f68a69dd283342b42a77bb401ca706
AbdulBasit0044 <abdulbasit0044@gmail.com>
2020-02-07 15:31:52 +0500

    Update main.dart

--- flutter_applications_the_net_ninja/myapp/lib/main.dart
+++ flutter_applications_the_net_ninja/myapp/lib/main.dart
@@ -1,36 +1,38 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('this is my second app'),
-      centerTitle: true,
-      backgroundColor: Colors.deepOrange[600],
-    ),
-    body: Center(
-      child: Text(
-        'This is the childish text',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.blueAccent,
-        ),
-      ),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text("Click"),
-      backgroundColor:Colors.deepOrange[600],
-    ),
-  )
+  home:Home(),
 ));
 
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('this is my second app'),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange[600],
+      ),
+      body: Center(
+        child: Text(
+          'This is the text',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.blueAccent,
+          ),
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text("Click"),
+        backgroundColor:Colors.deepOrange[600],
+      ),
+    );
   }
 }
