c583fd1c8de922fd4f79f7470f0ccdc715834ccc
AbdulBasit0044 <abdulbasit0044@gmail.com>
2020-04-12 21:24:57 +0500

    Comitting containers design.

--- destini_challenge/lib/story.dart
+++ destini_challenge/lib/story.dart
@@ -1,7 +1,9 @@
+import 'story_brain.dart';
+
 class Story {
   var storyTitle;
   var choice1;
   var choice2;
 
-  Story({storyTitle, choice1, choice2});
+  Story({this.storyTitle, this.choice1, this.choice2});
 }
